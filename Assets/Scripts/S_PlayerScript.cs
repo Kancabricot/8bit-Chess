580422e28feded4727a0e42c9c77fd5960209da1
Kancabricot <timothee.fontan@gmail.com>
2023-04-20 00:03:43 +0200

    Initial push et mise en place player movement

---
+++ Assets/Scripts/S_PlayerScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class S_PlayerScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0f;
+    [SerializeField] private Rigidbody2D rb;
+
+    private Vector2 MoveDirection;
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    private void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        MoveDirection = new Vector2 (moveX, moveY).normalized; 
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(MoveDirection.x * moveSpeed, MoveDirection.y * moveSpeed);
+    }
+}
